8cba0a73b4a3d6401fe3381c73fe07c3763b848c
JunuJIN <161922junu@gmail.com>
2023-07-14 13:10:48 +0900

    extract 0.4

--- extract0.py
+++ extract0.py
@@ -1,56 +1,25 @@
 ## pdf, epub에서 텍스트를 긁어오는 함수
-def read_pdf(fname):
-    import pdfplumber
+def read_pdf(file_path):
     import re
+    import fitz
 
-    pattern = r"[^ㄱ-ㅣ가-힣a-zA-Z\s\d\.,?!'%()\\\"“”‘’]"
-
-    pdf = pdfplumber.open(fname)
-    x_len = pdf.pages[0].mediabox[2]  # pdf 가로 길이
-    y_len = pdf.pages[0].mediabox[3]
-
-    tmp_list = []  # character에 대한 정보를 담을 json 그릇
-    for tmp in pdf.pages:
-        tmp_list.append(tmp.chars)
-    # tmp_list에 chr에 관한 json들이 덕지덕지존재한다.
-    # [[page1], [page2], [page3], [page4], ]와 관련된 json들을 담고 있다.
-
+    doc = fitz.open(path)
     text = ""
-    # 글자를 담을 최종그릇
 
-    for i in tmp_list:
-        # chr에 대한 정보를 페이지당 묶어서 가지고 있기 때문에
-        for j in i:
-            # 이제 각 chr에 대한 json에 대해서 논한다.
-            # chr의 위치에 대한 조건을 걸고,
-            if (
-                j["x0"] > 0
-                and j["x0"] < x_len
-                and j["x1"] > 0
-                and j["x1"] < x_len
-                and j["y0"] > 0
-                and j["y0"] < y_len
-                and j["y1"] > 0
-                and j["y1"] < y_len
-                and j["top"] > 0
-                and j["top"] < y_len
-                and j["bottom"] > 0
-                and j["bottom"] < y_len
-            ):
-                # pdfplumber가 인식하지 못하는 text는 고려하지 않는다.
-                if "cid" in j["text"]:
-                    pass
-                else:
-                    # 뽑아낸 텍스트에 대해서 전처리
-                    clean_text = re.sub(pattern, "", j["text"])
-                    clean_text = re.sub("“", '"', clean_text)
-                    clean_text = re.sub("”", '"', clean_text)
-                    clean_text = re.sub("’", "'", clean_text)
-                    clean_text = re.sub("‘", "'", clean_text)
-                    clean_text = re.sub("[\uf000-\uFFFF]|[\x01-\x09]", "", clean_text)
-                    text += clean_text
+    for i in range(len(doc)):
+        text += doc.get_page_text(i)
+    clean_text = re.sub("\d{0,3}\n\d{0,3}", "", text)
+    clean_text = re.sub("“", '"', clean_text)
+    clean_text = re.sub("”", '"', clean_text)
+    clean_text = re.sub("’", "'", clean_text)
+    clean_text = re.sub("‘", "'", clean_text)
 
-    return text
+    refined = re.findall("(?<=[^가-힣])[가-힣0-9\,\\s]+[\.\?\!]", clean_text)
+    processed = [element.strip() for element in refined if re.search("[가-힣]", element)]
+    text_list = [t for t in processed if len(t.strip()) > 5]
+    final = text_list[int(len(text_list) * 0.3): int(len(text_list) * 0.7)]
+
+    return final
 
 
 def read_epub(file_path):
@@ -69,7 +38,7 @@ def read_epub(file_path):
     refined = re.findall("(?<=[^가-힣])[가-힣0-9\,\\s]+[\.\?\!]", raw_text)
     processed = [element.strip() for element in refined if re.search("[가-힣]", element)]
     text_list = [t for t in processed if len(t.strip()) > 5]
-    final = text_list[int(len(text_list) * 0.15) : int(len(text_list) * 0.85)]
+    final = text_list[int(len(text_list) * 0.15): int(len(text_list) * 0.85)]
     # Remove '\n' from the result
     final = [text.replace("\n", "") for text in final]
     final = [text.replace("\t", " ") for text in final]
